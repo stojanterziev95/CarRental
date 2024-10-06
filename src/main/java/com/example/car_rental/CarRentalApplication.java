179f32b5a425e8ee16f1d0fbf164afa7930ed612
stojan <stole.t@outlook.com>
2024-10-06 20:31:49 +0200

    Small fix

--- src/main/java/com/example/car_rental/CarRentalApplication.java
+++ src/main/java/com/example/car_rental/CarRentalApplication.java
@@ -2,10 +2,12 @@ package com.example.car_rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.car_rental.repository")
+@EntityScan(basePackages = "com.example.car_rental.models")
 public class CarRentalApplication {
 
     public static void main(String[] args) {
